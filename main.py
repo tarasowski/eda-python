ffe144d8e75a4cb668082b586a58ff808473c2cc
tarasowski <dimitri+github@tarasowski.de>
2019-11-22 17:11:47 +0100

    starting of the project

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('./train.csv', sep=',')
+
+
+df_train.columns
+
